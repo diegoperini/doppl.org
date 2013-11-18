9785b46b4824f2cb6d53ab0f30fde547b9278f87
diegoperini <diego@dperini.com>
2013-11-18 02:50:43 +0200

    Added index,php and diary entries

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Doppl - Data Oriented Parallel Programming Language</title>
+    <meta name="description" content="Doppl is a programming language to write concurrent logic on shared memory natively. It utilizes data oriented design to fully benefit from hardware caches and effective scheduling." />
+    <meta name="keywords" content="Engineering,Programming,Programming Language,Concurrency,Multithreading,Multitasking" />
+    <meta name="author" content="Diego PERINI" />
+    <meta charset="UTF-8" />
+</head>
+<body>
+    <h1>Doppl</h1>
+    <p>Data Oriented Parallel Programming Language</p>
+    <a href="devdiary/DopplDevelopmentIntroduction.html">Introduction</a>
+    <ol>
+        <li><a href="devdiary/DopplDevelopmentIteration1.html">Task Definitions, State Definitions, Grammar</a></li>
+        <li><a href="devdiary/DopplDevelopmentIteration2.html">Literals, Primitive Data Types, Task Members</a></li>
+        <li><a href="devdiary/DopplDevelopmentIteration3.html">Access Modifiers, Assignment Operator</a></li>
+        <li><a href="devdiary/DopplDevelopmentIteration4.html">Boolean Type, Boolean Literals, Logic Operators</a></li>
+        <li><a href="devdiary/DopplDevelopmentIteration5.html">Arithmetic, Relational and Binary Operators</a></li>
+        <li><a href="devdiary/DopplDevelopmentIteration6.html">String Concatenation Operator</a></li>
+        <li><a href="devdiary/DopplDevelopmentIteration7.html">Standard Input and Output, once Access Modifier</a></li>
+        <li><a href="devdiary/DopplDevelopmentIteration8.html">State Members</a></li>
+        <li><a href="devdiary/DopplDevelopmentIteration9.html">States in Detail, State Transition Operators</a></li>
+        <li><a href="devdiary/DopplDevelopmentIteration10.html">Target Language for Compilation, Open Doppl Project</a></li>
+        <li><a href="devdiary/DopplDevelopmentIteration11.html">Future Members, States as Members, Closure States</a></li>
+    </ol>
+    <p>Doppl is a new programming language that aims providing a natural syntax for implementing parallel algorithms,<br />
+    designing data structures for shared memory applications and automated message passing among multiple tasks.<br />
+    The name is an abbreviation of `data oriented parallel programming language`.</p>
+    <p>Doppl development is logged as diary entries which extend, improve or remove feautes that are introduced by<br />
+    previous ones. Diary entries are called iterations and numbered by their versions. Once the language standard becomes<br />
+    mature enough and the compiler is relatively stable, this site will replace itself with a proper web app which will<br />
+    include the language standard(completely rewritten, a structured paper), a friendly documentation, a bug tracker,<br />
+    a mail group, the project source, milestones, a discussion platform and a blog.</p>
+    <p>Doppl is created and actively developed by <a href="http://dperini.com">Diego Perini</a>.</p>
+    <p>Last updated: 2013-11-18</p>
+    <p>All rights reserved.</p>
+</body>
+</html>
